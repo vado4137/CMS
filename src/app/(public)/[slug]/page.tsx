e3c7eb8c07596dd3cc169a91fa880e35dfef0f59
Vado Haze <joda80@outlook.de>
2026-04-23 12:05:12 +0200

    fix for Railway

--- src/app/(public)/[slug]/page.tsx
+++ src/app/(public)/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from "next/navigation";
 import { BlockRenderer } from "@/components/cms/BlockRenderer";
 import { CMSBlock } from "@/types/cms";
 
+export const dynamic = "force-dynamic";
+
 export default async function PublicFactionPage(props: { 
   params: Promise<{ slug: string }> 
 }) {
